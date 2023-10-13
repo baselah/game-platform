30fba910c861d389088590f27caf872c3608dd8d
basel alhawari <basel.ah.991@gmail.com>
2023-10-14 00:10:15 +0300

    add search for games

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,18 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/Ag1_09.jpg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-function NavBar() {
+
+
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+function NavBar({onSearch}:Props) {
   return (
     <HStack justifyContent={"space-between"} padding="10px">
       <Image src={logo} boxSize="60px"></Image>
       <Text whiteSpace={"nowrap"}>BsBs Games</Text>
-      <SearchInput></SearchInput>
+      <SearchInput onSearch={onSearch}></SearchInput>
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,11 +1,31 @@
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
-import { BsSearch } from 'react-icons/bs'
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
 
-export default function SearchInput() {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+export default function SearchInput({ onSearch }: Props) {
+  const ref = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup>
-    <InputLeftElement children={<BsSearch></BsSearch>}></InputLeftElement>
-   <Input borderRadius={20} placeholder='search games...' src='' variant={'filled'}></Input>
-   </InputGroup>
-  )
+    <form
+      style={{ width: "100%" }}
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch></BsSearch>}></InputLeftElement>
+        <Input
+          borderRadius={20}
+          placeholder="search games..."
+          src=""
+          variant={"filled"}
+          ref={ref}
+        ></Input>
+      </InputGroup>
+    </form>
+  );
 }
